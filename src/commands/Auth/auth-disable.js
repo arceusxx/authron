b374f803d84d53870213b9fdcfda09c2ebd618c1
arceus <96443442+arceusxx@users.noreply.github.com>
2023-06-06 19:34:04 +0200

    Code embelli

--- src/commands/Auth/auth-disable.js
+++ src/commands/Auth/auth-disable.js
@@ -1,80 +1,94 @@
-const { SlashCommandBuilder, PermissionsBitField, EmbedBuilder } = require('discord.js');
+const {
+  SlashCommandBuilder,
+  PermissionsBitField,
+  EmbedBuilder
+} = require('discord.js');
 const authSchema = require('../../schemas/authSchema');
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName('auth-disable')
-  .setDescription('Désactiver l\'authentification à emojis.')
-  .setDMPermission(false),
+     .setName('auth-disable')
+     .setDescription('Désactiver l\'authentification à emojis.')
+     .setDMPermission(false),
 
   async execute(interaction) {
-    if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
-      const errorEmbed = new EmbedBuilder()
-        .setTitle('🔒 Authentification Échouée')
-        .setDescription('> ❌  La permission `GERER_LES_SALONS` est requise pour désactiver l\'authentification à emojis.')
-        .setTimestamp()
-        .setFooter({
-          text: interaction.user.tag,
-          iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
-        })
-        .setColor('#da4744');
+     if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+        const errorEmbed = new EmbedBuilder()
+           .setTitle('🔒 Authentification Échouée')
+           .setDescription('> ❌  La permission `GERER_LES_SALONS` est requise pour désactiver l\'authentification à emojis.')
+           .setTimestamp()
+           .setFooter({
+              text: interaction.user.tag,
+              iconURL: interaction.user.displayAvatarURL({
+                 dynamic: true
+              }),
+           })
+           .setColor('#da4744');
 
-      return await interaction.reply({
-        embeds: [errorEmbed],
-        ephemeral: true
-      });
-    }
+        return await interaction.reply({
+           embeds: [errorEmbed],
+           ephemeral: true
+        });
+     }
 
-    const existingAuth = await authSchema.findOne({ Guild: interaction.guild.id });
+     const existingAuth = await authSchema.findOne({
+        Guild: interaction.guild.id
+     });
 
-    if (!existingAuth) {
-      const noAuthEmbed = new EmbedBuilder()
-        .setTitle('🔒 Authentification Échouée')
-        .setDescription('> ❌  Aucune authentification à emojis n\'est en place.')
-        .setTimestamp()
-        .setFooter({
-          text: interaction.user.tag,
-          iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
-        })
-        .setColor('#da4744');
+     if (!existingAuth) {
+        const noAuthEmbed = new EmbedBuilder()
+           .setTitle('🔒 Authentification Échouée')
+           .setDescription('> ❌  Aucune authentification à emojis n\'est en place.')
+           .setTimestamp()
+           .setFooter({
+              text: interaction.user.tag,
+              iconURL: interaction.user.displayAvatarURL({
+                 dynamic: true
+              }),
+           })
+           .setColor('#da4744');
 
-      return await interaction.reply({
-        embeds: [noAuthEmbed],
-        ephemeral: true
-      });
-    }
-    
-    authSchema.deleteMany({ Guild: interaction.guild.id }, async (err, data) => {
-      const successEmbed = new EmbedBuilder()
-        .setTitle('🔓 Authentification Réussie')
-        .setDescription('> ✅  L\'authentification à emojis a été désactivée avec succès !')
-        .setTimestamp()
-        .setFooter({
-          text: interaction.user.tag,
-          iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
-        })
-        .setColor('#01ba01');
+        return await interaction.reply({
+           embeds: [noAuthEmbed],
+           ephemeral: true
+        });
+     }
 
-      await interaction.reply({
-        embeds: [successEmbed],
-        ephemeral: true
-      });
+     authSchema.deleteMany({
+        Guild: interaction.guild.id
+     }, async (err, data) => {
+        const successEmbed = new EmbedBuilder()
+           .setTitle('🔓 Authentification Réussie')
+           .setDescription('> ✅  L\'authentification à emojis a été désactivée avec succès !')
+           .setTimestamp()
+           .setFooter({
+              text: interaction.user.tag,
+              iconURL: interaction.user.displayAvatarURL({
+                 dynamic: true
+              }),
+           })
+           .setColor('#01ba01');
 
-      const channel = interaction.channel;
+        await interaction.reply({
+           embeds: [successEmbed],
+           ephemeral: true
+        });
 
-      try {
-        const messages = await channel.messages.fetch();
-        const authSetupMessage = messages.find((msg) => 
-                msg.embeds.length && msg.embeds[0].title === '🔒 Authentification'
-        );
+        const channel = interaction.channel;
 
-        if (authSetupMessage) {
-          await authSetupMessage.delete();
+        try {
+           const messages = await channel.messages.fetch();
+           const authSetupMessage = messages.find((msg) =>
+              msg.embeds.length && msg.embeds[0].title === '🔒 Authentification'
+           );
+
+           if (authSetupMessage) {
+              await authSetupMessage.delete();
+           }
+        } catch (error) {
+           console.error('Erreur de suppression pour `auth-setup`:', error);
+
         }
-      } catch (error) {
-        console.error('Erreur de suppression pour `auth-setup`:', error);
-
-      }
-    });
+     });
   }
 };
