25f37900c16dbeb36ac3304eb9670288817bc98b
arceus <96443442+arceusxx@users.noreply.github.com>
2023-06-06 19:33:32 +0200

    Code embelli

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,28 +1,29 @@
-const { Interaction } = require("discord.js");
-
-module.exports = {
+const {
+    Interaction
+ } = require("discord.js");
+ 
+ module.exports = {
     name: 'interactionCreate',
     async execute(interaction, client) {
-        if (!interaction.isCommand()) return;
-
-        const command = client.commands.get(interaction.commandName);
-
-        if (!command) return
-        
-        try{
-
-
-            await command.execute(interaction, client);
-        } catch (error) {
-            console.log(error);
-            await interaction.reply({
-                content: 'Une erreur s\'est produite lors de l\'exécution de cette commande!', 
-                ephemeral: true
-            });
-        } 
-
+       if (!interaction.isCommand()) return;
+ 
+       const command = client.commands.get(interaction.commandName);
+ 
+       if (!command) return
+ 
+       try {
+ 
+ 
+          await command.execute(interaction, client);
+       } catch (error) {
+          console.log(error);
+          await interaction.reply({
+             content: 'Une erreur s\'est produite lors de l\'exécution de cette commande!',
+             ephemeral: true
+          });
+       }
+ 
     },
-    
-
-
-};
+ 
+ 
+ };
